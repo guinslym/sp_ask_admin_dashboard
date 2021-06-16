bbd866468cdc3dbf7cad2e944eb78e02352d02bd
Guinsly Mondesir <guinslym@gmail.com>
2021-06-15 22:55:09 -0400

    docker

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,5 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (cd sp_ask_admin_dashboard; python manage.py createsuperuser --no-input)
 fi
-(cd sp_ask_admin_dashboard; gunicorn project_config.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
+(cd project_config; gunicorn project_config.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
